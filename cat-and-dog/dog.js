547e9717b175b2c42a8cd9efa4e950dceedc8dc5
David van der Linde <80853616+optimizer-david@users.noreply.github.com>
2024-07-29 07:52:51 +0200

    Create dog.js

---
+++ cat-and-dog/dog.js
@@ -0,0 +1,10 @@
+console.log("***** DOG *****");
+console.log(" ");
+
+console.log(",-.___,-. ");
+console.log("\_/_ _\_/ ");
+console.log("  )O_O( ");
+console.log(" { (_) } ");
+console.log("  `-^-' ");
+
+console.log(" ");
